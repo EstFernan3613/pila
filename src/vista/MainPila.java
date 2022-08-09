588a659c0953ed18231b86a88f1b00011b26ffc9
EstFernan3613 <juan_est.fernandez@uao.edu.co>
2022-08-09 10:07:38 -0500

    Initial commit

---
+++ src/vista/MainPila.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import datos.Productos;
+import modelo.Pila;
+
+/**
+ *
+ * @author juan_est.fernandez
+ */
+public class MainPila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pila<Productos> pilaP= new Pila<>();       
+        pilaP.apilar(new Productos("arroz", 2, 5000));
+        pilaP.apilar(new Productos("panela", 4, 12000));
+        pilaP.apilar(new Productos("panalhuevos", 1, 12000));
+        pilaP.apilar(new Productos("frijoles", 3, 15000));
+        System.out.println("Pila \n"+pilaP.toString());
+        
+        pilaP.desapilar();
+        System.out.println("Pila \n"+pilaP.toString());
+    }           
+}
